188af61e9fc35a60a79f1f6d012d3f40974501c1
skchronicles <kuhnsa2@vcu.edu>
2021-07-21 17:42:56 -0400

    Adding build command to create FAI and extract transcript sequences

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+from __future__ import print_function
 from shutil import copytree
-import os, subprocess
+import os, subprocess, sys
 
 def permissions(parser, filename, *args, **kwargs):
     """Checks permissions using os.access() to see the user is authorized to access
@@ -39,13 +40,29 @@ def exists(testpath):
     return does_exist
 
 
-def initialize(output_path):
+def ln(files, outdir):
+    """Creates symlinks for files to an output directory.
+    @param files list[<str>]:
+        List of filenames
+    @param outdir <str>:
+        Destination or output directory to create symlinks
+    """
+    # Create symlinks for each file in the output directory
+    for file in files:
+        ln = os.path.join(outdir, os.path.basename(file))
+        if not exists(ln):
+                os.symlink(os.path.abspath(os.path.realpath(file)), ln)
+        
+
+def initialize(output_path, links=[]):
     """Initialize the output directory. If user provides a output
     directory path that already exists on the filesystem as a file 
     (small chance of happening but possible), a OSError is raised. If the
     output directory PATH already EXISTS, it will not try to create the directory.
     @param output_path <str>:
         Pipeline output path, created if it does not exist
+    @param links list[<str>]:
+        List of files to symlink into output_path
     """
     if not exists(output_path):
         # Pipeline output directory does not exist on filesystem
@@ -58,6 +75,10 @@ def initialize(output_path):
         Please run {} again with a different --output PATH.
         """.format(sys.argv[0])
         )
+    
+    # Create symlinks for each file in the output directory
+    ln(links, output_path)
+    
 
 
 def which(cmd, path=None):
@@ -79,6 +100,55 @@ def which(cmd, path=None):
         if executable and is_not_directory:
             return True
     return False
+
+
+def err(*message, **kwargs):
+    """Prints any provided args to standard error.
+    kwargs can be provided to modify print functions 
+    behavior.
+    @param message <any>:
+        Values printed to standard error
+    @params kwargs <print()>
+        Key words to modify print function behavior
+    """
+    print(*message, file=sys.stderr, **kwargs)
+
+
+
+def fatal(*message, **kwargs):
+    """Prints any provided args to standard error
+    and exits with an exit code of 1.
+    @param message <any>:
+        Values printed to standard error
+    @params kwargs <print()>
+        Key words to modify print function behavior
+    """
+    err(*message, **kwargs)
+    sys.exit(1)
+
+
+def require(cmds, suggestions, path=None):
+    """Enforces an executable is in $PATH
+    @param cmds list[<str>]:
+        List of executable names to check
+    @param suggestions list[<str>]:
+        Name of module to suggest loading for a given index
+        in param cmd.
+    @param path list[<str>]]:
+        Optional list of PATHs to check [default: $PATH]
+    """
+    error = False
+    for i in range(len(cmds)):
+        available = which(cmds[i])
+        if not available:
+            error = True
+            err("""\n\tFatal: {} is not in $PATH and is required during runtime!
+            └── Solution: please 'module load {}' and run again!""".format(cmds[i], suggestions[i])
+            )
+
+    if error: sys.exit(1)
+    
+    return 
 
 
 def safe_copy(source, target, resources = []):

fb6fe2bc16739df2469d6ddc3c6b22ecb490ae54
skchronicles <kuhnsa2@vcu.edu>
2021-08-02 17:26:30 -0400

    Adding handler for determining correct reader for a given MAF-like file

--- src/reader.py
+++ src/reader.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 from __future__ import print_function
 import pandas as pd
-import sys
+import sys, os
 
 
 def fasta(filename):
@@ -36,6 +36,42 @@ def fasta(filename):
             yield chrom, sequence
 
 
+def maf(filename, subset=[], skip='#', **kwargs):
+    """Reads in an MAF-like file as a dataframe. Determines the 
+    correct handler for reading in a given MAF file. Supports reading
+    in TSV files (.tsv, .txt, .text, .vcf, or .maf), CSV files (.csv), 
+    and excel files (.xls, .xlsx, .xlsm, .xlsb, .odf, .ods, .odt ). 
+    The subset option allows a users to only select a few columns 
+    given a list of column names.
+    @param filename <str>:
+        Path of an MAF-like file to read and parse
+    @param subset list[<str>]:
+        List of column names which can be used to subset the df
+    @param skip <str>:
+        Skips over line starting with this character
+    @params kwargs <read_excel()>
+        Key words to modify pandas.read_excel() function behavior
+    @return <pandas dataframe>:
+        dataframe with spreadsheet contents
+    """
+    # Get file extension
+    extension = os.path.splitext(filename)[-1].lower()
+
+    # Assign a handler to read in the file
+    if extension in ['.xls', '.xlsx', '.xlsm', '.xlsb', '.odf', '.ods', '.odt']:
+        # Read in as an excel file
+        return excel(filename, subset, skip, **kwargs)
+    elif extension in ['.csv']:
+        # Read in as an CSV file
+        return csv(filename, subset, skip, **kwargs)
+    else:
+        # Default to reading in as an TSV file
+        # Tab is the normal delimeter for MAF or VCF files
+        # MAF files usually have one of the following
+        # extensions: '.tsv', '.txt', '.text', '.vcf', '.maf'
+        return tsv(filename, subset, skip, **kwargs)
+
+
 def excel(filename, subset=[], skip='#', **kwargs):
     """Reads in an excel file as a dataframe. The subset option
     allows a users to only select a few columns given a list of 
@@ -56,6 +92,50 @@ def excel(filename, subset=[], skip='#', **kwargs):
         return pd.read_excel(filename, comment=skip, **kwargs)[subset]
     
     return pd.read_excel(filename, comment=skip, **kwargs)
+
+
+def tsv(filename, subset=[], skip='#', **kwargs):
+    """Reads in an TSV file as a dataframe. The subset option
+    allows a users to only select a few columns given a list of 
+    column names.
+    @param filename <str>:
+        Path of an TSV file to read and parse
+    @param subset list[<str>]:
+        List of column names which can be used to subset the df
+    @param skip <str>:
+        Skips over line starting with this character
+    @params kwargs <read_excel()>
+        Key words to modify pandas.read_excel() function behavior
+    @return <pandas dataframe>:
+        dataframe with spreadsheet contents
+    """
+    if subset: 
+        # 'Transcript_ID','HGVSc','Hugo_Symbol', 'Gene'
+        return pd.read_table(filename, comment=skip, **kwargs)[subset]
+    
+    return pd.read_table(filename, comment=skip, **kwargs)
+
+
+def csv(filename, subset=[], skip='#', **kwargs):
+    """Reads in an CSV file as a dataframe. The subset option
+    allows a users to only select a few columns given a list of 
+    column names.
+    @param filename <str>:
+        Path of an CSV file to read and parse
+    @param subset list[<str>]:
+        List of column names which can be used to subset the df
+    @param skip <str>:
+        Skips over line starting with this character
+    @params kwargs <read_excel()>
+        Key words to modify pandas.read_excel() function behavior
+    @return <pandas dataframe>:
+        dataframe with spreadsheet contents
+    """
+    if subset: 
+        # 'Transcript_ID','HGVSc','Hugo_Symbol', 'Gene'
+        return pd.read_csv(filename, comment=skip, **kwargs)[subset]
+    
+    return pd.read_csv(filename, comment=skip, **kwargs)
 
 
 def main():
